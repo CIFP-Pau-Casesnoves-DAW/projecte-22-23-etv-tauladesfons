bc9e3d7c8437ea92d1ed266bdd8f850d7bd8b294
jaumetruyols <jaumetruyols@paucasesnovescifp.cat>
2023-01-12 20:56:23 +0100

    MODELO FOTOGRAFIA Y LOGIN EMPEZADO

---
+++ app/Models/Fotografia.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Fotografia extends Model
+{
+    use HasFactory;
+    protected $table = 'FOTOGRAFIES';
+    protected $primaryKey = 'ID_FOTO, FK_ID_ALLOTJAMENT';
+    public $timestamps = false;
+    public $incrementing = false;
+    protected $fillable = [
+        'ID_FOTO',
+        'FOTO',
+        'FK_ID_ALLOTJAMENT'
+    ];
+    public function allotjament()
+    {
+        return $this->belongsTo('App\Models\Allotjament', 'FK_ID_ALLOTJAMENT', 'ID_ALLOTJAMENT');
+    }
+
+}
