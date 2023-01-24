3d714a4c572c0db6daeb7710dcdca9e193f825c5
Isaac Palou <isaacpalou@paucasesnovescifp.cat>
2023-01-24 18:58:23 +0100

    perf: :zap: Canviar nom funcions del controlador i fer l'insert d'una manera correcte

--- app/Http/Controllers/Traduccio_serveiController.php
+++ app/Http/Controllers/Traduccio_serveiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Traduccio_servei;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class Traduccio_serveiController extends Controller
 {
@@ -13,27 +14,11 @@ class Traduccio_serveiController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    // ! GET all
+    public function getTraduccionsServeis()
     {
         $tuples = Traduccio_servei::all();
         return response()->json(['status' => 'success', 'result' => $tuples], 200);
-    }
-
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $traduccio_servei = new Traduccio_servei();
-        $traduccio_servei->FK_ID_SERVEI = $request->FK_ID_SERVEI;
-        $traduccio_servei->FK_ID_IDIOMA = $request->FK_ID_IDIOMA;
-        $traduccio_servei->TRADUCCIO_SERVEI = $request->TRADUCCIO_SERVEI;
-        $traduccio_servei->save();
-        return response()->json(['status' => 'success', 'result' => $traduccio_servei], 200);
     }
 
     /**
@@ -42,7 +27,8 @@ class Traduccio_serveiController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id_servei, $id_idioma)
+    // ! GET de una en especific
+    public function getTraduccioServei($id_servei, $id_idioma)
     {
         try {
             $traduccio_servei = Traduccio_servei::where('FK_ID_SERVEI', $id_servei)->where('FK_ID_IDIOMA', $id_idioma)->first();
@@ -53,11 +39,31 @@ class Traduccio_serveiController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Store a newly created resource in storage.
      *
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+    // ! INSERT
+    public function insertTraduccioServei(Request $request){
+        $validacio=Validator::make($request->all(),[
+            'FK_ID_SERVEI' => 'exists:serveis,ID_SERVEI',
+            'FK_ID_IDIOMA' => 'exists:idiomes,ID_IDIOMA'
+        ]);
+        if (!$validacio->fails()) {
+            $traduccio_servei = new Traduccio_servei();
+            $traduccio_servei->FK_ID_SERVEI = $request->FK_ID_SERVEI;
+            $traduccio_servei->FK_ID_IDIOMA = $request->FK_ID_IDIOMA;
+            $traduccio_servei->TRADUCCIO_SERVEI = $request->TRADUCCIO_SERVEI;
+            if ($traduccio_servei->save()) {
+                return response()->json(['status'=> 'Creat','result'=> $traduccio_servei],200);
+            }else {
+                return response()->json(['status'=> 'Error creant']);
+            }
+        }else {
+            return response()->json(['status'=> 'Error:servei o idioma inexistents']);
+        }
+    }
 
     /**
      * Update the specified resource in storage.
@@ -66,7 +72,9 @@ class Traduccio_serveiController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id){
+
+     // ! UPDATE
+    public function updateTraduccioServei(Request $request, $id_servei,$id_idioma){
         //
     }
 
@@ -78,7 +86,9 @@ class Traduccio_serveiController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id_servei, $id_idioma)
+
+    // ! DELETE
+    public function deleteTraduccioServei($id_servei, $id_idioma)
     {
         $traduccio_servei = Traduccio_servei::where('FK_ID_SERVEI', $id_servei)->where('FK_ID_IDIOMA', $id_idioma)->delete();
 
@@ -88,4 +98,10 @@ class Traduccio_serveiController extends Controller
             return response()->json(['status' => 'No trobat'], 404);
         }
     }
+
+    // public function createValidator():array{
+    //     return [
+    //         "FK_ID_SERVEI" => 
+    //     ]
+    // }
 }
