44bcf08b337bcd05743521717b39ba474747b7fb
Maria <mariamargalidaferrer@paucasesnovescifp.cat>
2023-01-26 16:43:57 +0100

    fix: eliminar atribut "fotografia"

--- app/Models/Allotjament.php
+++ app/Models/Allotjament.php
@@ -20,7 +20,6 @@ class Allotjament extends Model
         'LLITS',
         'PERSONES',
         'BANYS',
-        'FOTOGRAFIES',
         'ADRECA',
         'DESTACAT',
         'VALORACIO_GLOBAL',
