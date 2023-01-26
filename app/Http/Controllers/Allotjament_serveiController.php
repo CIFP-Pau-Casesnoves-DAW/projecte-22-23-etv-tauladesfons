e3b9d51aba97deab2157bc0b1af615d480cc82ab
jaumetruyols <jaumetruyols@paucasesnovescifp.cat>
2023-01-26 01:48:44 +0100

    allotjament_Servei documentado, no funciona el update

--- app/Http/Controllers/Allotjament_serveiController.php
+++ app/Http/Controllers/Allotjament_serveiController.php
@@ -3,16 +3,52 @@
 namespace App\Http\Controllers;
 
 use App\Models\Allotjament_servei;
-use Dotenv\Validator;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use \Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use OpenApi\Annotations as OA;
 
+/**
+ *@OA\Tag(name="Allotjament_serveis")
+ */
 class Allotjament_serveiController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
+     */
+    /**
+     * @OA\Get(
+     *     path="/allotjaments_serveis",
+     *     summary="Mostra els allotjament_serveis",
+     *     tags={"Allotjament_serveis"},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Retorna tots els allotjament_serveis"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="No hi ha allotjament_serveis"
+     *     )
+     * )
+     * @OA\Schema(
+     *     schema="Allotjament_servei",
+     *     @OA\Property(
+     *     property="FK_ID_ALLOT",
+     *     type="integer",
+     *     description="ID de l'allotjament",
+     *     example="1"
+     *    ),
+     *     @OA\Property(
+     *     property="FK_ID_SERVEI",
+     *     type="integer",
+     *     description="ID del servei",
+     *     example="1"
+     *   )
+     * )
      */
     public function index()
     {
@@ -21,38 +57,45 @@ class Allotjament_serveiController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
+    /**
+     * @OA\Post(
+     *     path="/allotjaments_serveis",
+     *     summary="Crea un allotjament_servei",
+     *     tags={"Allotjament_serveis"},
+     *     @OA\RequestBody(
+     *         description="Allotjament_servei a crear",
+     *         required=true,
+     *         @OA\JsonContent(ref="#/components/schemas/Allotjament_servei"),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Allotjament_servei creat"
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Error creant allotjament_servei"
+     *     )
+     * )
+     */
     public function store(Request $request)
     {
-        $reglevalidacio=Validator::make($request->all(), [
+        $reglesvalidacio = Validator::make($request->all(), [
             'FK_ID_ALLOT' => 'exists:ALLOTJAMENTS,ID_ALLOTJAMENT',
             'FK_ID_SERVEI' => 'exists:SERVEIS,ID_SERVEI'
         ]);
-        if (!$reglevalidacio->fails()) {
+        if (!$reglesvalidacio->fails()) {
             $tuple = new Allotjament_servei();
             $tuple->FK_ID_ALLOT = $request->FK_ID_ALLOT;
             $tuple->FK_ID_SERVEI = $request->FK_ID_SERVEI;
-            if ($tuple->save()) {
-                return response()->json(['status' => 'Afegit','result' => $tuple], 200);
-            } else {
-                return response()->json(['status' => 'Error'], 500);
-            }
+            $tuple->save();
+            return response()->json($tuple);
         } else {
-            return response()->json(['status' => 'Error creant'], 500);
+            return response()->json(['error' => 'Bad request'], 400);
         }
     }
 
@@ -62,27 +105,49 @@ class Allotjament_serveiController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        try {
-            $allot_serveis = Allotjament_servei::findOrFail($id);
-            return response()->json($allot_serveis);
-        } catch (ModelNotFoundException $e) {
-            return response()->json(['error' => 'Allotjament_servei not found'], 404);
-        }
-    }
-
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @OA\Get(
+     *     path="/allotjaments_serveis/{id_allot}/{id_servei}",
+     *     summary="Mostra un allotjament_servei",
+     *     tags={"Allotjament_serveis"},
+     *     @OA\Parameter(
+     *         description="ID allotjament",
+     *         in="path",
+     *         name="id_allot",
+     *         required=true,
+     *         @OA\Schema(
+     *           type="integer"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         description="ID servei",
+     *         in="path",
+     *         name="id_servei",
+     *         required=false,
+     *         @OA\Schema(
+     *           type="integer"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Retorna un allotjament_servei"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="No existeix allotjament_servei"
+     *     )
+     * )
      */
-    public function edit($id)
-    {
-        //
-    }
 
+    public function show($id_allot, $id_servei)
+    {
+       try{
+           $allotjament_servei = Allotjament_servei::where('FK_ID_ALLOT', $id_allot)->where('FK_ID_SERVEI', $id_servei)->firstOrFail();
+           return response()->json("Allotjament_servei: " . $allotjament_servei . " amb ID allotjament: " . $id_allot . " i ID servei: " . $id_servei);
+       } catch (ModelNotFoundException $e) {
+           return response()->json(['error' => 'No existeix allotjament_servei amb aquest ID allotjament: ' . $id_allot . ' i ID servei: ' . $id_servei], 404);
+       }
+    }
     /**
      * Update the specified resource in storage.
      *
@@ -90,23 +155,62 @@ class Allotjament_serveiController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    /**
+     * @OA\Put(
+     *     path="/allotjaments_serveis/put/{id_allot}/{id_servei}",
+     *     summary="Actualitza un allotjament_servei",
+     *     tags={"Allotjament_serveis"},
+     *     @OA\Parameter(
+     *         description="ID allotjament",
+     *         in="path",
+     *         name="id_allot",
+     *         required=true,
+     *         @OA\Schema(
+     *           type="integer"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         description="ID servei",
+     *         in="path",
+     *         name="id_servei",
+     *         required=false,
+     *         @OA\Schema(
+     *           type="integer"
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *         description="Allotjament_servei a actualitzar",
+     *         required=true,
+     *         @OA\JsonContent(ref="#/components/schemas/Allotjament_servei"),
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Allotjament_servei actualitzat"
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Error actualitzant allotjament_servei"
+     *     )
+     * )
+     */
+    public function update(Request $request, $id_allot, $id_servei)
     {
         $reglesvalidacio = Validator::make($request->all(), [
             'FK_ID_ALLOT' => 'exists:ALLOTJAMENTS,ID_ALLOTJAMENT',
             'FK_ID_SERVEI' => 'exists:SERVEIS,ID_SERVEI'
         ]);
         if (!$reglesvalidacio->fails()) {
-            $tuple = Allotjament_servei::findOrFail($id);
-            $tuple->FK_ID_ALLOT = $request->FK_ID_ALLOT;
-            $tuple->FK_ID_SERVEI = $request->FK_ID_SERVEI;
-            if ($tuple->save()) {
-                return response()->json(['status' => 'Actualitzat','result' => $tuple], 200);
-            } else {
-                return response()->json(['status' => 'Error'], 500);
+            try {
+                $allotjament_servei = Allotjament_servei::where('FK_ID_ALLOT', $id_allot)->where('FK_ID_SERVEI', $id_servei)->firstOrFail();
+                $allotjament_servei->FK_ID_ALLOT = $request->FK_ID_ALLOT;
+                $allotjament_servei->FK_ID_SERVEI = $request->FK_ID_SERVEI;
+                $allotjament_servei->update();
+                return response()->json($allotjament_servei);
+            } catch (ModelNotFoundException $e) {
+                return response()->json(['error' => 'No existeix allotjament_servei amb aquest ID allotjament: ' . $id_allot . ' i ID servei: ' . $id_servei], 404);
             }
         } else {
-            return response()->json(['status' => 'Error actualitzant'], 500);
+            return response()->json(['error' => 'Bad request'], 400);
         }
     }
 
@@ -116,6 +220,39 @@ class Allotjament_serveiController extends Controller
      * @param $FK_ID_ALLOT
      * @param $FK_ID_SERVEI
      * @return Response
+     */
+    /**
+     * @OA\Delete(
+     *     path="/allotjaments_serveis/destroy/{id_allot}/{id_servei}",
+     *     summary="Elimina un allotjament_servei",
+     *     tags={"Allotjament_serveis"},
+     *     @OA\Parameter(
+     *         description="ID allotjament",
+     *         in="path",
+     *         name="id_allot",
+     *         required=true,
+     *         @OA\Schema(
+     *           type="integer"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         description="ID servei",
+     *         in="path",
+     *         name="id_servei",
+     *         required=false,
+     *         @OA\Schema(
+     *           type="integer"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Allotjament_servei eliminat"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Error eliminant allotjament_servei"
+     *     )
+     * )
      */
     public function destroy($FK_ID_ALLOT, $FK_ID_SERVEI)
     {
