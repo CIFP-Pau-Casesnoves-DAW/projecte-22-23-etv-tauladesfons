8a2a6bfa540cc25fa97c3b13879a0a435252046f
Isaac Palou <isaacpalou@paucasesnovescifp.cat>
2023-01-30 15:39:19 +0100

    Canvis per fer canvis en local

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -13,7 +13,7 @@ use Illuminate\Routing\Controller as BaseController;
 * @OA\Contact( name="Isaac Palou.",email="isaacpalou@paucasesnovescifp.cat")
 * )
 *
-* @OA\Server(url="http://www.etvtauladesfons.com/api")
+* @OA\Server(url="http://localhost:8080/api")
 *
 * @OAS\SecurityScheme(
 * securityScheme="bearerAuth",
