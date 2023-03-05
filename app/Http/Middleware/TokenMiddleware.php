bc22bbab7055bdfe6614e5141e0b8081424c3d32
isaacpaloug <isaacpalou@paucasesnovescifp.cat>
2023-03-05 19:34:02 +0100

    fix: :bug: Millorar middleware per tal de que envii al request si es admin i la id de l'usuari logeat

--- app/Http/Middleware/TokenMiddleware.php
+++ app/Http/Middleware/TokenMiddleware.php
@@ -11,16 +11,23 @@ class TokenMiddleware
     public function handle(Request $request, Closure $next)
     {
         if ($request->header('Authorization')) { // Hem rebut el header d’autorització?
-            $key = explode(' ', $request->header('Authorization')); // Esperam un token 'Bearer token'
-            $token = 'x'; // Els usuaris que no han fet mai un login tenen un token buit;
-            if (count($key) == 2) {
-                $token = $key[1]; // key[0]->Bearer key[1]→token
+            $key = explode(' ',$request->header('Authorization'));
+            if (count($key)!=2) {
+                return response()->json(["error" => "Access no autoritzat"], 401);
             }
-            $user = Usuari::where('TOKEN', $token)->first();
-            if (!empty($user)) {
-                return $next($request); // Usuari trobat. Token correcta. Continuam am la petició
-            } else {
-                return response()->json(['error' => 'Accés no autoritzat'], 401); // token incorrecta
+            $token=$key[1];
+
+            if ($token === null || trim($token) === "") {
+                return response()->json(['error' => "Acces no autoritzat"],401);
+            }
+            $usuari = Usuari::where("TOKEN",$token)->first();
+            if (!empty($usuari)) {
+                $admin = $usuari->ADMINISTRADOR;
+                $id = $usuari->ID_USUARI;
+                $request->merge(["esAdministrador"=>$admin, "validat_id" => $id]);
+                return($next($request));
+            }else {
+                return response()->json(['error' => 'Accés no autoritzat'], 401);
             }
         } else {
             return response()->json(['error' => 'Token no rebut'], 401);
